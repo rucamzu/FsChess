7b10483f525aabc7079f2f17797b18461bd994e1
Rubén Campos Zurriaga <ruben.campos.zurriaga@gmail.com>
2024-05-02 23:34:29 +1000

    feat: add common (un)curry functions

---
+++ src/FsChess.Common/Functions.fs
@@ -0,0 +1,14 @@
+﻿module FsChess.Common.Functions
+
+let curry2 f = fun a b -> f (a, b)
+let curry3 f = fun a b c -> f (a, b, c)
+let curry4 f = fun a b c d -> f (a, b, c, d)
+let curry5 f = fun a b c d e -> f (a, b, c, d, e)
+let curry = curry2
+
+
+let uncurry2 f = fun (a, b) -> f a b
+let uncurry3 f = fun (a, b, c) -> f a b c
+let uncurry4 f = fun (a, b, c, d) -> f a b c d
+let uncurry5 f = fun (a, b, c, d, e) -> f a b c d e
+let uncurry = uncurry2
